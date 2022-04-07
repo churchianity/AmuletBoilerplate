8267e2438c68d6e42e6e3574365b083ed54be3bf
churchianity <hayashi.nicholas@gmail.com>
2022-04-07 19:28:47 -0400

    add some more canonical colors

--- color.lua
+++ color.lua
@@ -1,4 +1,5 @@
 
+-- table of canonical colors used in the game.
 COLORS = {
     TRANSPARENT         = vec4(0.6),
     TRANSPARENT1        = vec4(0.4),
@@ -6,5 +7,13 @@ COLORS = {
     -- tones
     WHITE               = vec4(1, 1, 1, 1),
     BLACK               = vec4(0, 0, 0, 1),
+
+    -- hues
+    RED                 = vec4(1, 0, 0, 1),
+    GREEN               = vec4(0, 1, 0, 1),
+    BLUE                = vec4(0, 0, 1, 1),
+    YELLOW              = vec4(1, 1, 0, 1),
+    MAGENTA             = vec4(1, 0, 1, 1),
+    CYAN                = vec4(0, 1, 1, 1),
 }
 
